2fb1720f58e83db82fa449f39023a37b96b3c07e
Aidan Gray <aidancgray@gmail.com>
2024-02-27 14:37:22 -0500

    log output to usb

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,4 +5,15 @@ source $srcDir/venv/bin/activate
 
 now=`date +"%Y-%m-%dT%H.%M.%S"`
 
-python $srcDir/load_splash.py $srcDir/splash_screen 0.05
+nohup python $srcDir/load_splash.py $srcDir/splash_screen 0.05
+
+if mountpoint -q /mnt/usbLog
+then
+    printf "USB Drive mounted\n"
+    echo "USB Drive mounted\n" >> /mnt/usbLog/$now.log
+    # nohup python $srcDir/main.py --logLevel=20 --updateTime=34 --gain=10000 "$@" >& /mnt/usbLog/$now.log &
+else
+    printf "USB Drive NOT mounted\n"
+    # nohup python $srcDir/main.py --logLevel=20 --updateTime=34 --gain=10000 >/dev/null 2>&1 &
+fi
+
