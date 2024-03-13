84bf7b7a16c91bf0ffc938411ccb4359dcb25e35
Aidan Gray <aidancgray@gmail.com>
2024-03-13 15:24:43 -0400

    updated script for zodPlot

--- kill_services.sh
+++ kill_services.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-printf "killing VIM-BCAST/PARLL services...\n"
-for pid in $(pgrep -f 'main_.*.py'); do kill $pid; done
+printf "killing ZODPLOT services...\n"
+for pid in $(pgrep -f 'main.*.py'); do kill $pid; done
 printf "done\n"
