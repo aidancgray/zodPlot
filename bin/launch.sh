6bc46dd5ce1f9e4d7245fe2a0145fdbd698647a5
Aidan Gray <aidancgray@gmail.com>
2024-03-13 15:26:52 -0400

    lotta changes...

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 srcDir=/home/idg/src/zodPlot
 source $srcDir/venv/bin/activate
 
-now=`date +"%Y-%m-%dT%H.%M.%S"`
-
-nohup python $srcDir/load_splash.py $srcDir/splash_screen 0.05
+now=`date +"%Y-%m-%d_%H-%M-%S"`
 
 if mountpoint -q /mnt/usbLog
 then
-    printf "USB Drive mounted\n"
-    echo "USB Drive mounted\n" >> /mnt/usbLog/$now.log
-    # nohup python $srcDir/main.py --logLevel=20 --updateTime=34 --gain=10000 "$@" >& /mnt/usbLog/$now.log &
+    printf "USB Drive mounted\n" >> /mnt/usbLog/$now.log
+    nohup python $srcDir/main.py \
+        --logLevel=20 \
+        --updateTime=34 \
+        --gain=10000 \
+        --imgLog=/mnt/usbLog/ \
+        "$@" >> /mnt/usbLog/$now.log &
 else
     printf "USB Drive NOT mounted\n"
-    # nohup python $srcDir/main.py --logLevel=20 --updateTime=34 --gain=10000 >/dev/null 2>&1 &
+    nohup python $srcDir/main.py \
+        --logLevel=20 \
+        --updateTime=34 \
+        --gain=10000 \
+        >/dev/null 2>&1 &
 fi
 
+nohup python $srcDir/load_splash.py $srcDir/splash_screen 0.05
