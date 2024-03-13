60218d96a3f1edca679414341b058cd7325a26ae
Aidan Gray <aidancgray@gmail.com>
2024-03-13 19:42:15 -0400

    still making changes here

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,22 +5,33 @@ source $srcDir/venv/bin/activate
 
 now=`date +"%Y-%m-%d_%H-%M-%S"`
 
+nohup python $srcDir/load_splash.py $srcDir/splash_screen 0.05 >/dev/null 2>&1 &
+
+nohup python $srcDir/wait_for_start.py >/dev/null 2>&1
+startCode=$?
+
+if (( $startCode == 1 ))
+then
+    printf "SECRET START!\n"
+elif (( $startCode == 2 ))
+then
+    printf "SECRET ~OTHER~ START!\n"
+fi
+
 if mountpoint -q /mnt/usbLog
 then
-    printf "USB Drive mounted\n" >> /mnt/usbLog/$now.log
+    printf "USB Drive mounted\nStarting ZODPlot processes\n"
     nohup python $srcDir/main.py \
         --logLevel=20 \
         --updateTime=34 \
         --gain=10000 \
         --imgLog=/mnt/usbLog/ \
-        "$@" >> /mnt/usbLog/$now.log &
+        "$@" >& /mnt/usbLog/$now.log &
 else
-    printf "USB Drive NOT mounted\n"
+    printf "USB Drive NOT mounted\nStarting ZODPlot processes\n"
     nohup python $srcDir/main.py \
         --logLevel=20 \
         --updateTime=34 \
         --gain=10000 \
         >/dev/null 2>&1 &
 fi
-
-nohup python $srcDir/load_splash.py $srcDir/splash_screen 0.05
